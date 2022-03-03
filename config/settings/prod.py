1fd75b1f5e265e6cc16766fd0a50690751e2f9c6
puri <pirralet@gmail.com>
2022-03-04 02:51:49 +0900

    도메인 연결

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.39.49.197']
+ALLOWED_HOSTS = ['3.39.49.197', 'porrti14.duckdns.org']
 STATIC_ROOT = BASE_DIR / 'pybo/static/'
 STATICFILES_DIRS = []
 DEBUG = False
