c4dd64ad3f18124d44f2b4ccb95b8afc9a71d938
puri <pirralet@gmail.com>
2022-03-12 09:01:59 +0900

    회원정보 수정 기능 추가

--- common/forms.py
+++ common/forms.py
@@ -1,11 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 # 계정 생성시 사용할 USERFORM
 class UserForm(UserCreationForm):
     email = forms.EmailField(label='이메일')
-
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+    email = forms.EmailField(label='이메일')
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
--- common/views.py
+++ common/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from common.forms import UserForm
+from .forms import CustomUserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 def signup(request):
     # 계정생성
@@ -17,6 +21,30 @@ def signup(request):
         # get 요청일 경우 계정생성 화면 리턴
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})
+
+def update(request):
+    # 회원 정보 수정
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance = request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = CustomUserChangeForm(instance = request.user)
+
+    return render(request, 'common/update.html', {'form': form})
+
+@login_required
+def password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('index')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'common/password.html', {'form': form})
 
 def page_not_found(request, exception):
     # 오류 페이지 - 404 not found
